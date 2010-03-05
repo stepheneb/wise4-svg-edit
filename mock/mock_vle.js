42cb7fd878806a2e62f186ca9ce17e974cd12718
Richard Klancer <rklancer@dhcp199.concord.org>
2010-03-05 15:06:53 -0500

    Mocked loading node and student data; default drawing now shows; snapshots panel can be opened (but doesn't quite work right.

--- mock/mock_vle.js
+++ mock/mock_vle.js
@@ -5,7 +5,12 @@
 // http://confluence.concord.org/display/CSP/Notes+on+startup+sequence+of+WISE4-embedded+svg-edit
 
 nodeId = "(standalone version of wise4-svg-edit)";
-    
+
+if (!console) {
+    // "console = console || ..." notation doesn't work as console is a property with no setter
+    console = { log: function () {} };
+}
+
 var node = {
     contentPanel: {
         window: window
@@ -24,7 +29,7 @@ var node = {
                 // mock out this URL
                 // the url returned here will be passed to $.getJSON in SVGDRAW.prototype.loadModules 
                 // (see line 56, svgdraw.js)
-                return "";
+                return "mock/mock_node_config.json";
             }
         };
     }
@@ -104,19 +109,63 @@ var scriptloader = (function () {
             loadNextUnloadedScript();
             if (allScriptsAreLoaded()) {
                 // notify the svg-edit component that all scripts are loaded
-                loadContentAfterScriptsLoad(node);
-                loadIconsAfterPageLoad();   
+                loadContentAfterScriptsLoad(node);  
             }
         }
     };   
 }());
 
 
+var vle = {
+    annotations: null,
+    
+    getConfig: function () {
+        return {
+            getConfigParam: function (arg) {
+                console.log("vle.getConfig().getConfigParam() called with argument: " + arg);
+                if (arg=='mode') {
+                    return 'run';
+                }
+            }
+        };
+    },    
+    
+    getUserAndClassInfo: function () {
+        return {
+            getWorkgroupId: function () {},
+            getTeacherWorkgroupId: function () {},
+            getPeriodId: function () {}
+        };
+    },
+    
+    
+    getCurrentNode: function () {
+    },
+    
+    getLatestStateForCurrentNode: function () {
+        return {
+            description: "A page.",
+            selected: -1,
+            snapTotal: 1,
+            snapshots: [{
+                id: 1,
+                svg: '<svg width="600" height="450" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">  <g>  <title>Layer 1</title>  <rect x="102.5" y="57" width="92" height="116" id="svg_1" fill="#FF0000" stroke="#000000" stroke-width="5"/>  </g> </svg>'
+            }],
+            svgString: '<svg width="600" height="450" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">  <g>  <title>Layer 1</title>  <rect stroke-width="5" stroke="#000000" fill="#FF0000" id="svg_2" height="147" width="147" y="109.5" x="231"/> </g> </svg>'
+        };
+    },
+  
+    saveState: function () {
+    }
+};
+
+
+
 (function () {
     var ajax = $.ajax;
     
     $.ajax = function(settings) {
-        var error = settings.error;
+        var error = settings.error || function () {};
         settings.error = function () {
             console.log("$.ajax error() callback called for url: " + settings.url);
             error();
@@ -127,8 +176,24 @@ var scriptloader = (function () {
     };
 }());
 
+// fixes for possible circular dependencies: methods called by self-executing function in svg-editor.js before objects
+// are defined in svg_edit_setup() (also in svg-editor.js)
 
 // mock out $.pref for line 2586 of svg-editor.js
 $.pref = function(key, val) {
-    console.log("calling $.pref with key: "+key+" val: "+val);
+    var undefined;
+
+    console.log("$.pref called with key: " + key + " val: " + val + " before being (re)defined.");
+    
+    if (key=='iconsize' && val==undefined) {
+        return('m');
+    }
+};
+
+// $.svgCanvas for line 2591 of svg-editor.js
+
+var svgCanvas = {
+    setIconSize: function (size) {
+        console.log("svgCanvas.setIconSize called with argument: " + size + " before being (re)defined.");
+    }
 };
